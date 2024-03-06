3ad14eefceb6e354d56cd8e0441e70be25c0d5c4
Zurheb <behber84@icloud.com>
2024-03-06 17:51:43 +0300

    number3

--- src/MainPage/MiddleOfThePage/Creater/Creater.jsx
+++ src/MainPage/MiddleOfThePage/Creater/Creater.jsx
@@ -56,7 +56,7 @@ export const Creater = () => {
             </div>
           </div>
         </div>
-        <div className="flex justify-center mt-12 gap-8">
+        <div className="flex flex-wrap justify-center mt-12 gap-8">
           <div className="max-w-sm text-center">
             <h2 className="font-bold text-lg text-gray-800 mb-2">
               Prototyping
