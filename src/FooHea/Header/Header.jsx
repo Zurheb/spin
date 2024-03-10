e0d777548557a13ab4097d8a89116c3954570198
Zurheb <behber84@icloud.com>
2024-03-10 14:40:27 +0300

    add anomation scale

--- src/FooHea/Header/Header.jsx
+++ src/FooHea/Header/Header.jsx
@@ -19,19 +19,19 @@ export function StickyNavbar() {
 
   const navList = (
     <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 mx-auto">
-      <Typography as="li" variant="h6" className="p-1  font-normal">
+      <Typography as="li" variant="h6" className="p-1 hover:scale-110  font-normal">
         <a href="/about" className="flex items-center">
           About
         </a>
       </Typography>
 
-      <Typography as="li" variant="h6" className="p-1 font-normal">
+      <Typography as="li" variant="h6" className="p-1 hover:scale-110 font-normal">
         <a href="/getting-started" className="flex items-center">
           Documentation
         </a>
       </Typography>
 
-      <Typography as="li" variant="h6" className="p-1 font-normal">
+      <Typography as="li" variant="h6" className="p-1 hover:scale-110 font-normal">
         <a href="/color" className="flex items-center">
           Components
         </a>
@@ -43,15 +43,15 @@ export function StickyNavbar() {
     <header className="max-w-[1600px] mx-auto my-0 sticky top-0  z-10">
       <div className="max-h-[768px] bg-black  ">
         <Navbar className=" h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-black">
-          <div className="flex items-center justify-between ">
+          <div className="flex  items-center justify-between ">
             <div className="flex gap-24">
               <Typography
                 as="a"
                 href="/"
-                className="mr-4 cursor-pointer py-1.5 font-medium"
+                className="mr-4  cursor-pointer py-1.5 font-medium"
               >
                 <img
-                  className="w-auto h-auto max-h-4 lg:max-h-6"
+                  className="w-auto hover:scale-105 h-auto max-h-4 lg:max-h-6"
                   src="https://themewagon.gitlab.io/spin/assets/img/logos/spin_logo.png"
                   alt=""
                 />
@@ -101,13 +101,13 @@ export function StickyNavbar() {
             <div className="hidden lg:flex items-center gap-5">
               <Button
                 variant="text"
-                className="text-white bg-blue-gray-900 hover:bg-yellow-600"
+                className="text-white hover:scale-105 bg-blue-gray-900 hover:bg-yellow-600"
               >
                 <span>Subscribe</span>
               </Button>
               <Button
                 variant="text"
-                className="text-white bg-blue-gray-900 hover:bg-blue-500"
+                className="text-white hover:scale-105 bg-blue-gray-900 hover:bg-blue-500"
               >
                 <span>Get started-it's free</span>
               </Button>
